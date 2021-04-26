f5fc5b75c2e15e7220d9bd1b15585b196659fd6c
Austin LaShure <austinglashure@gmail.com>
2021-04-26 05:16:08 -0400

    added basic, create, deal, shuffle, retrieve functionality

---
+++ deck.py
@@ -0,0 +1,26 @@
+import random
+SUITS = "SDCH"
+FACES = "23456789TJQKA"
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for foo in FACES:
+            for bar in SUITS:
+                self.card = [foo, bar]
+                self.deck.append(self.card)
+    
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+    
+    def deal_card(self):
+        return self.deck.pop([0])
+    
+    def retrieve_card(self, card):
+        self.deck.append(card)
+    
+    def get_card_value(self):
+        return self.card[0]
+    
+    def get_card_suit(self):
+        return self.card[1]
