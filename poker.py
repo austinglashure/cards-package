1a23bf9ea861a51bbeb43e8145d78a359a86b6d7
Austin LaShure <austinglashure@gmail.com>
2021-04-26 06:05:54 -0400

    added a sorter function to the Evaluator class

--- poker.py
+++ poker.py
@@ -1,4 +1,5 @@
-import deck
+import cards_package.deck as deck
+import copy
 
 HAND_LIST = ["High Card", "Two of a Kind", "Two Pair", "Three of a Kind", "Straight", "Flush", "Full House",
              "Four of a Kind", "Straight Flush"]
@@ -12,4 +13,20 @@ def get_suit(card):
     return card[1]
 
 
+class Evaluator:
+    def __init__(self):
+        self.cards_pool = []
 
+    def load_card(self, card):
+        self.cards_pool.append(card)
+    
+    def sort_cards(self):
+        self.sorted_cards_pool = copy.deepcopy(self.cards_pool)
+        for i in range(len(self.sorted_cards_pool)-1):
+            for j in range(len(self.sorted_cards_pool)-1):
+                if deck.FACES.index(self.sorted_cards_pool[j][0]) > deck.FACES.index(self.sorted_cards_pool[j + 1][0]):
+                    foo = self.sorted_cards_pool[j]
+                    self.sorted_cards_pool[j] = self.sorted_cards_pool[j + 1]
+                    self.sorted_cards_pool[j + 1] = foo
+        print(self.cards_pool)
+        print(self.sorted_cards_pool)
