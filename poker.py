298f913cd555f22ec529cd67141bd3f317ae7375
Austin LaShure <austinglashure@gmail.com>
2021-04-26 05:45:22 -0400

    building module, will have tests for poker hands

---
+++ poker.py
@@ -0,0 +1,15 @@
+import deck
+
+HAND_LIST = ["High Card", "Two of a Kind", "Two Pair", "Three of a Kind", "Straight", "Flush", "Full House",
+             "Four of a Kind", "Straight Flush"]
+
+
+def get_value(card):
+    return card[0]
+
+
+def get_suit(card):
+    return card[1]
+
+
+
